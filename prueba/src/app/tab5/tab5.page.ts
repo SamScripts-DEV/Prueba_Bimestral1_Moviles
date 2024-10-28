7986f8255338472ca087b50c7ff3667ce41e9be8
Samuel Cuti <robsamuel.cuti@epn.edu.ec>
2024-10-28 02:16:32 -0500

    Finalizacion

--- prueba/src/app/tab5/tab5.page.ts
+++ prueba/src/app/tab5/tab5.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Filesystem, Directory, Encoding } from '@capacitor/filesystem';
 
 @Component({
   selector: 'app-tab5',
@@ -8,29 +9,68 @@ import { Component } from '@angular/core';
 export class Tab5Page {
 
   nota: string = '';
+  archivoNota: string = 'notas.txt';
+  notasGuardadas: string[] = [];
 
-  constructor() {
-    this.cargarTexto();
-  }
+  constructor() {}
 
-  viewWhenEnter(){
-    const textoGuardado = localStorage.getItem('item')
-    if(textoGuardado){
-      this.nota=textoGuardado
+  async guardarNota() {
+    if (this.nota.trim() !== '') {
+      try {
+        await Filesystem.writeFile({
+          path: this.archivoNota,
+          data: this.nota,
+          directory: Directory.Documents,
+          encoding: Encoding.UTF8,
+        });
+
+        this.nota = '';
+        alert('Nota guardada correctamente');
+        
+      } catch (error) {
+        console.error('Error al guardar la nota', error);
+        alert('Error al guardar la nota');
+        
+      }
+      
     }
   }
 
-  guardarTexto(){
-    localStorage.setItem('notaGuardada', this.nota);
-    alert('Nota Guardada')
-  }
-
-  cargarTexto(){
-    const textoGuardar = localStorage.getItem('notaGuardada');
-    if(textoGuardar){
-      this.nota = textoGuardar;
+  async cargarNotas() {
+    try {
+      const result = await Filesystem.readFile({
+        path: this.archivoNota,
+        directory: Directory.Documents,
+        encoding: Encoding.UTF8,
+      });
+      if (typeof result.data === 'string') {
+        this.notasGuardadas = [result.data]; // Almacena la nota en un array
+      } else if (result.data instanceof Blob) {
+        const text = await this.blobToString(result.data);
+        this.notasGuardadas = [text]; // Almacena la nota en un array
+      }
+    } catch (error) {
+      console.error('Error al cargar las notas', error);
+      alert('Error al cargar las notas');
     }
   }
 
+  private async blobToString(blob: Blob): Promise<string> {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        resolve(reader.result as string);
+      };
+      reader.onerror = reject;
+      reader.readAsText(blob);
+    });
+
+  }
+
+ 
+  
+
+ 
+  
 
 }
