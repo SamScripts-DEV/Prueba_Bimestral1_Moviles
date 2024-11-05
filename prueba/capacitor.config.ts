d508673db461b7da23bb5de6a9bd40e9533fedca
Samuel Cuti <robsamuel.cuti@epn.edu.ec>
2024-11-04 20:11:39 -0500

    AppMejorada
    Splash-Screen
    Icon Personalisable

--- prueba/capacitor.config.ts
+++ prueba/capacitor.config.ts
@@ -1,9 +1,26 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'prueba',
-  webDir: 'www'
-};
+  appId: 'com.appmejorada.app',
+  appName: 'AppMejorada',
+  webDir: 'www',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 0,
+      launchAutoHide: true,
+      backgroundColor: "#736caa",
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: false,
+      androidSpinnerStyle: "large",
+      iosSpinnerStyle: "small",
+      spinnerColor: "#999999",
+      splashFullScreen: false,
+      splashImmersive: false,
+      layoutName: "launch_screen",
+      useDialog: false,
+    },
+  },
+}
 
 export default config;
